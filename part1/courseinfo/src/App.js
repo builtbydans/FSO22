2333f48adf69662ed99085cd3b9fcf359efad0cd
Danish Shafi <danish.shafi@live.co.uk>
2022-01-06 19:07:58 +0000

    Skeleton React app created for prop management

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,16 +8,8 @@ const Header = (props) => {
   )
 } 
 
-// Exercise 1.2 was not worded appropriately to understand what the task actually needed.
-
-const Content = (props) => {  
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+const Content = (props) => {
+    
   const Part = (props) => {
     return(
     <>
@@ -25,11 +17,12 @@ const Content = (props) => {
     </>
     )
   }
+
   return(
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
     </div>
   )
 } 
@@ -37,19 +30,47 @@ const Content = (props) => {
 const Total = (props) => {
   return(
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 } 
 
-const App = () => {
-  const course = 'Half Stack application development'
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-      <Header course={course} />
-      <Content />
-      <Total total="31" />
+      <p>
+        Hello {name}, you are {age} years old. So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+  
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+      <Hello name="Danish" age="29" />
     </div>
   )
 }
